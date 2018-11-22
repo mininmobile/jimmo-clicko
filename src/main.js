5d54d34f30c80d38316e9a4e64ab6345aeb07593
MininMobile <notminin@pm.me>
2018-11-22 22:10:25 +0000

    add base

--- src/main.js
+++ src/main.js
@@ -1,9 +1,35 @@
-let amount = document.getElementById("amount");
-let clicker = document.getElementById("clicker");
+let snotCounter = document.getElementById("snot-counter");
+let spsCounter = document.getElementById("sps-counter");
+let c = document.getElementById("clicker");
 
+let updateInterval;
+let spsInterval;
+
+let sps = 0;
 let snot = 0;
 
-clicker.addEventListener("click", () => {
-	snot += 1;
-	amount.innerText = snot;
-});
+let upgrades = {
+	finger: 0,
+	nostril: 0
+}
+
+let clicker = {
+	click() {
+		snot += 1;
+		update();
+	},
+}
+
+updateInterval = setInterval(() => update("default"), 1);
+spsInterval = setInterval(() => snot += sps / 10, 100);
+
+c.addEventListener("click", clicker.click);
+
+function update(type = "default") {
+	switch (type) {
+		case "default": {
+			snotCounter.innerText = Math.round(snot);
+			spsCounter.innerText = Math.round(sps);
+		} break;
+	}
+}
